9fdcbe747582631f479c4b5542de7cf02b0af057
mentix02 <manan.yadav02@gmail.com>
2019-06-23 00:52:43 +0530

    added a series detail page

---
+++ src/pages/SeriesDetail.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+import { Jumbotron, Container } from 'react-bootstrap';
+import TutorialList from "../components/TutorialList";
+
+class SeriesDetail extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      series: {
+        tutorial_count: null
+      }
+    }
+  }
+
+  getSeries() {
+
+    const slug = this.props.match.params.slug;
+    const url = `/api/series/detail/${slug}/`;
+
+    axios.get(url).then(
+      res => {
+        this.setState({
+          series: res.data
+        })
+      }
+    ).catch(
+      err => {
+        console.log(err.response);
+      }
+    );
+
+  }
+
+  componentDidMount() {
+    this.getSeries();
+  }
+
+  render() {
+
+    const {series} = this.state;
+
+    return (
+      <div>
+        <Jumbotron>
+          <Container>
+            <h1 className="display-4">
+              {series.name}
+            </h1>
+            <p>
+              {series.description}
+            </p>
+          </Container>
+        </Jumbotron>
+        { this.state.series.tutorial_count ? <TutorialList series={series.name} url={`/api/series/detail/${series.slug}/tutorials/`} />
+        : <h2 className="text-center">No tutorials yet.</h2>}
+      </div>
+    );
+  }
+
+}
+
+export default SeriesDetail;
