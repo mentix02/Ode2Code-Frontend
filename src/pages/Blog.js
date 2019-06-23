d7f58b2f195bb85c3d62ffaf57dae989b89ef817
mentix02 <manan.yadav02@gmail.com>
2019-06-24 00:13:45 +0530

    fixed link to /p/:slug for detail

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -85,7 +85,7 @@ class Blog extends Component {
                     <Card.Text>
                       {post.description}
                     </Card.Text>
-                    <LinkContainer to={`/post/detail/${post.slug}`}>
+                    <LinkContainer to={`/p/${post.slug}`}>
                       <Button variant="primary">Read more</Button>
                     </LinkContainer>
                   </Card.Body>
