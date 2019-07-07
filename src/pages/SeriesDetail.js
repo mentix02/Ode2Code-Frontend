753d764d0b60905386277012aa24636fcf23d1cf
mentix02 <manan.yadav02@gmail.com>
2019-07-07 18:24:14 +0530

    added author details to SeriesDetail.js

--- src/pages/SeriesDetail.js
+++ src/pages/SeriesDetail.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import {toTitleCase} from '../utils/text';
-import { Jumbotron, Container } from 'react-bootstrap';
+import { toTitleCase } from '../utils/text';
+import { isAuthorsSeries } from '../utils/auth';
 import TutorialList from '../components/TutorialList';
+import { Jumbotron, Container, Button } from 'react-bootstrap';
 
 class SeriesDetail extends Component {
 
@@ -12,6 +13,7 @@ class SeriesDetail extends Component {
     super(props);
     this.state = {
       series: {
+        creator: '',
         tutorial_count: null
       }
     }
@@ -43,6 +45,7 @@ class SeriesDetail extends Component {
   render() {
 
     const {series} = this.state;
+    const creator = series.creator;
 
     return (
       <div>
@@ -51,12 +54,15 @@ class SeriesDetail extends Component {
             <h1 className="display-4">
               {series.name}
             </h1>
-            <Link to={`/st/${series.type_of}`}>
-              <h4 className="text-muted">{toTitleCase(series.type_of || "")}</h4>
-            </Link>
+            <h4 className="text-muted">
+              By <Link to={`/a/${creator}`}>{creator}</Link> under <Link to={`/st/${series.type_of}`}>{series.type_of}</Link>
+            </h4>
             <p>
               {series.description}
             </p>
+            {
+              isAuthorsSeries(creator) ? <Link className="btn btn-success" to="/new/tutorial/">write</Link> : ''
+            }
           </Container>
         </Jumbotron>
         { this.state.series.tutorial_count ? <TutorialList series_detail={true} series={series.name} url={`/api/series/detail/${series.slug}/tutorials/`} />
