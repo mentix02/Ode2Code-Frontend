240526bcf8d5d8972157d4b49edac31cbf3e2e9c
mentix02 <manan.yadav02@gmail.com>
2019-08-26 23:16:00 +0530

    page now redirects to the 404 page when slug is not found

--- src/pages/series/SeriesDetail.js
+++ src/pages/series/SeriesDetail.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-import {Link} from 'react-router-dom';
+import { toast } from 'react-toastify';
 import Tooltip from 'react-bootstrap/Tooltip';
 import Popover from 'react-bootstrap/Popover';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import { Link, Redirect } from 'react-router-dom';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 import { toTitleCase } from '../../utils/string';
@@ -17,7 +18,8 @@ class SeriesDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mounted: false,
+      loaded: false,
+      redirect: false,
       series: {
         name: '',
         slug: '',
@@ -40,14 +42,27 @@ class SeriesDetail extends Component {
   componentDidMount() {
     const slug = this.props.match.params.slug;
     get_series_detail(slug).then(data => {
-      this.setState({
-        series: data,
-        mounted: true
-      });
+      if (data.status) {
+        toast.error('Could not load series.', {
+          position: toast.POSITION.BOTTOM_CENTER
+        });
+        this.setState({
+          redirect: true
+        })
+      } else {
+        this.setState({
+          series: data,
+          loaded: true,
+        });
+      }
     });
   }
 
   render() {
+
+    if (this.state.redirect) {
+      return <Redirect to="/404" />
+    }
 
     const series = this.state.series;
     const username = series.creator.user.username;
@@ -71,26 +86,33 @@ class SeriesDetail extends Component {
       <div>
         <Jumbotron>
           <Container>
-            <h1>{series.name}</h1>
-            <hr/>
-            <h4 className="text-muted">
-              <small>
-                Started by
-                <OverlayTrigger placement="right" overlay={userDetailPopover}>
-                  <Link to={`/a/${username}`} style={linkNoStyle}> {username}</Link>
-                </OverlayTrigger>&nbsp;
-                {
-                  series.creator.authenticated ? <OverlayTrigger overlay={authenticatedToolTip}>
-                    <small className="fas fa-check text-dark" />
-                  </OverlayTrigger> : ''
-                } on <span dangerouslySetInnerHTML={{__html: timestamp}} /> under <Link to={`/st/${series.type_of}`}>
-                {toTitleCase(series.type_of)}</Link>
-              </small>
-            </h4>
-            <p>{series.description}</p>
+            {
+              this.state.loaded ?
+                <div>
+                  <h1>{series.name}</h1>
+                  <hr/>
+                  <h4 className="text-muted">
+                    <small>
+                      Started by
+                      <OverlayTrigger placement="right" overlay={userDetailPopover}>
+                        <Link to={`/a/${username}`} style={linkNoStyle}> {username}</Link>
+                      </OverlayTrigger>&nbsp;
+                      {
+                        series.creator.authenticated ? <OverlayTrigger overlay={authenticatedToolTip}>
+                          <small className="fas fa-check text-dark" />
+                        </OverlayTrigger> : ''
+                      } on <span dangerouslySetInnerHTML={{__html: timestamp}} /> under <Link to={`/st/${series.type_of}`}>
+                      {toTitleCase(series.type_of)}</Link>
+                    </small>
+                  </h4>
+                  <p>{series.description}</p>
+                </div>
+                :
+                <h1 className="text-danger text-center">Error 500</h1>
+            }
           </Container>
           {
-            this.state.mounted ?
+            this.state.loaded ?
               <TutorialList source={`/api/series/detail/${series.slug}/tutorials/`}/>
               :
               ''
